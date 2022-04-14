7d09e1d5169f761d4dee80300720f2f8ba03e93d
Usr-Unavailable <92661160+Usr-Unavailable@users.noreply.github.com>
2022-04-13 19:56:55 -0700

    Add files via upload

---
+++ challenge03.py
@@ -0,0 +1,16 @@
+layout = [
+    ["O","e","e",None, "e","o","e",None, "o","o","E"], # row 1
+    ["O","o","e",None, "e","o","e",None, "e","o","E"], # row 2
+    ["O","o","o",None, "o","o","e",None, "o","e","O"], # row 3
+    ["E","o","e",None, "e","o","e",None, "o","e","E"], # row 4 
+    ["E","e","o",None, "e","e","e",None, "o","o","E"], # row 5
+    ["O","e","e",None, "e","e","e",None, "e","e","E"]  # row 6
+]
+
+for i, row in enumerate(layout):
+    if row[0] == "E" and row[1] == "e": 
+        print("Left window seat is available for Dot in row ", i + 1)
+        break
+    elif row[-1] == "E" and row[-2] == "e": 
+        print("Right window seat is available for Dot in row ", i + 1)
+        break
